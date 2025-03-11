4ab1086f34c3cdfe2b3cf11caaf26eb7e5e85483
Zeyad Waleed <zeyadw526@gmail.com>
2025-03-11 22:58:24 +0200

    annotations

---
+++ annotations.ts
@@ -0,0 +1,11 @@
+// String
+const myName: string = 'Zeyad';
+console.log(myName);
+
+// Number
+const num: number = 12;
+console.log(num);
+
+// Boolean
+const isHard: boolean = false;
+console.log(isHard);
