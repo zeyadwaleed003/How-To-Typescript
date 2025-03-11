990efcf0ea5f8c6d3be9aa780abd74e14eab3461
Zeyad Waleed <zeyadw526@gmail.com>
2025-03-11 23:36:31 +0200

    Type Annotation

---
+++ 1. Annotations/anyType.ts
@@ -0,0 +1,8 @@
+let color: any = 'Red';
+console.log(typeof color);
+
+color = 2;
+console.log(typeof color);
+
+color = false;
+console.log(typeof color);
---
+++ 1. Annotations/typeInference.ts
@@ -0,0 +1,11 @@
+let tech = 'Typescript';
+
+// ERROR
+// tech = 12;
+
+const num1 = 11;
+const isMad = true;
+
+console.log(typeof tech);
+console.log(typeof num1);
+console.log(typeof isMad);
